16bfd0ea7a9cb55963a7bfeca412d89e033cf4c8
Akshay Dhobale <akshaysd592@gmail.com>
2022-11-08 12:16:15 +0530

    new file added gcd

---
+++ 2-D array problems/gcd_problems.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    if (b == 0)
+        return a;
+
+    while (a != b)
+    {
+        if (a > b)
+        {
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
+        }
+    }
+    if (a > 0)
+    {
+        return a;
+    }
+    else
+        return b;
+}
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int result = gcd(a, b);
+    cout << "The gcd of " << a << " and " << b << " is " << result << endl;
+}
